433bc4762ba1760126343dd1a34c1cd819e25337
I Was Framed <iwasframedcom@gmail.com>
2022-05-20 15:09:25 -0700

    added api package

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const { send } = require('express/lib/response');
-var fractional = require("./public/js/fraction.js");
-var prismjs = require("./public/js/prism.js");
+var Fraction = require('fractional').Fraction
+var nodemailer = require('nodemailer');
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,35 +28,79 @@ app.get('', (req, res) => {
     res.render('index')
 });
 
-app.get("/register", (req,res) => {
-    res.render('register', { title: 'Register'});
-
-});
-
  app.get("/about", (req,res) => {
     res.render('about', { title: 'About Page'});
 
 });
+
+app.get('/about', function(req, res) {
+    var locals = {
+      title: 'About',
+      description: 'About Us',
+      header: 'I Was Framed - About'
+    };
+    res.render('the-view', locals);
+  });
 
 app.get("/contact", (req,res) => {
     res.render('contact', { title: 'Contact Us'});
 
 });
 
+app.post('/contact', (req, res) => {
+    console.log(req.body)
+
+
+  const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 587,
+      // secure: true,
+      auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD
+      }
+  });
+
+
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.SMTP_USER,
+        subject:`Message from ${req.body.email}:  ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) =>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+  
+
 app.get("/documentation", (req,res) => {
     res.render('documentation', { title: 'Documentation'});
 
 });
 
-app.get("/measurements", (req,res,next) => {
-    res.render("measurements");
+app.get("/math", (req,res) => {
+    res.render('math', { title: 'Math Equations'});
 });
 
-app.get("/measurements/calculate", (req,res,) => {
-    res.render("measurements");
+app.get("/api", (req,res) => {
+    res.render('api', { title: 'API Instructions'});
+
 });
 
-app.post("/measurements/calculate", (req,res) => {
+app.get("/api/calculator", (req,res,) => {
+    res.render("calculator");
+});
+
+app.post("/api/calculator", (req,res) => {
     
     let FrameWidth = req.body.FrameWidth;
     let FrameWidthFraction = req.body.FrameWidthFraction
